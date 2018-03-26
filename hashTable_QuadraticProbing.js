1ed6c4db1f31d84c79707b2cd0592aa8fefa51a5
Tobi Bello <bello.tobi@outlook.com>
2018-03-26 15:13:41 +0100

    undo changes

--- hashTable_QuadraticProbing.js
+++ hashTable_QuadraticProbing.js
@@ -2,67 +2,42 @@
  * Hash Table - Quadratic Probing
 */
 
-var HashEntry = function (key, value) {
+var HashEntry = function(key, value) {
     this.key = key;
     this.value = value;
 }
 
-var HashTable = function (size = 127) {
+var HashTable = function(size) {
     this.array = new Array(size);
     this.count = 0;
 }
 
-HashTable.prototype.isFull = function () {
+HashTable.prototype.isFull= function() {
     return this.count === this.array.length;
 }
 
-HashTable.prototype.hashFunction = function (key) {
-    const keyType = typeof key;
+HashTable.prototype.insert = function(key, value) {
+    var i = base = key % this.array.length;
 
-    const getASCIISumOf = (text) => {
-        let sum = 0;
-        for (let i = 0; i <= text.length - 1; i++) {
-            sum += text.charCodeAt(i);
-        }
-        return sum;
-    }
-
-    switch (keyType) {
-        case "number":
-            return key % this.array.length;
-            break;
-        case "string":
-            return getASCIISumOf(key) % this.array.length;
-            break;
-        default:
-            throw "invalid key";
-    }
-}
-
-HashTable.prototype.insert = function (key, value) {
-    // get hash index
-    var i = base = this.hashFunction(key);
-
-    let step = 1;
+    var step = 1;
 
     // handle collision
     while (this.array[i]) {
-        i = (base + step * step) % this.array.length;
+        i = (base+step*step) % this.array.length;
         step++;
     }
 
     // insert
     this.array[i] = new HashEntry(key, value);
     this.count++;
-    return true;
 }
 
-HashTable.prototype.find = function (key) {
+HashTable.prototype.find = function(key) {
     // get hash index
-    var i = base = this.hashFunction(key);
+    var i = base = key % this.array.length;
 
     var step = 1;
-    while (true) {
+    while(true) {
         if (!this.array[i]) {
             return "not found"
         }
@@ -70,66 +45,37 @@ HashTable.prototype.find = function (key) {
             return this.array[i].value;
         }
         else {
-            i = (base + step * step) % this.array.length;
+            i = (base+step*step) % this.array.length;
         }
         step++;
     }
 }
 
-HashTable.prototype.remove = function (key) {
+HashTable.prototype.remove = function(key) {
     // get hash index
-    var i = base = this.hashFunction(key);
-
+    var i = base = key % this.array.length;
+    
     var step = 1;
-    while (true) {
+    while(true) {
         if (!this.array[i]) {
             return "not found"
         }
         else if (this.array[i].key === key) {
-            this.array[i] = undefined;
-            this.count--;
-            return true;
+            this.array[i] = -1;  
+            this.count--; 
             break;
         }
         else {
-            i = (base + step * step) % this.array.length;
+            i = (base+step*step) % this.array.length;
         }
         step++;
     }
 
 }
 
-const logOut = console.log;
-const logHashTable = (table) => {
-    console.log(table.array);
-}
-var table = new HashTable(255);
-
-logOut(table.insert("foo","bar"));
-logHashTable(table);
-
-logOut(table.insert(130805001, {
-    "name": "Tobi Bello",
-    "matricNumber": 130805001,
-}));
-logHashTable(table);
-
-logOut(table.insert("unilag", {
-    "name": "University of lagos",
-    "slug": "unilag",
-    "address":"Akoka, Yaba"
-}));
-logHashTable(table);
-
-logOut(table.insert("greetings", {
-    "english": "Hello",
-    "spanish": "Halo",
-    "french": "Bonjour",
-    "yoruba": "Bawo"
-}));
-logHashTable(table);
-
-logOut(table.remove("foo"));
-logHashTable(table);
-
-logOut(table.find("greetings").english);
+var table = new HashTable(5);
+table.insert(8, "hello");
+table.insert(3, "bye");
+table.insert(8, "hi");
+table.find(7);
+table.remove(3);
